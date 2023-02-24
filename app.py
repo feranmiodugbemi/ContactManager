72526b3b3b34d27c20b2c14e8dd5352b446f0ab6
feranmiodugbemi10 <feranmiodugbemi@gmail.com>
2023-02-24 13:11:40 +0100

    finished my form though ui is not that good

--- app.py
+++ app.py
@@ -1,10 +1,44 @@
-from flask import Flask, render_template
-import faunadb
+from flask import Flask, render_template, request, flash
+from faunadb import query as q
+from faunadb.objects import Ref
+from faunadb.client import FaunaClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 app = Flask(__name__)
 
-@app.route("/")
+client = FaunaClient(
+  secret=os.getenv('FAUNASECRET')
+)
+
+
+
+@app.route("/", methods=["POST", "GET"])
 def hello():
-    return render_template('index.html')
+    if request.method == "POST":
+        client.query(
+            q.create(
+                q.ref('collections/Users'),
+                {
+                    'data':{
+                        'id': q.new_id(),
+                        'name':"Feranmi",
+                        'occupation':'CEO',
+                        'address' : "Block 200, Alaka",
+                        'contact': "+234 816 043 5459",
+                        'email': 'feranmi@email.com'
+                    }
+                }
+            )
+        )
+    else:
+        return render_template('index.html')
+
+
+
+
+
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
