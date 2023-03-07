f97b0bc871aebf8f4f72164b6b52c9cac8c80d85
feranmiodugbemi10 <feranmiodugbemi@gmail.com>
2023-03-07 23:37:04 +0100

    made some new changes, might break app

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def hello():
         address = request.form.get('address', default='')
         contact = request.form.get('contact')
         email = request.form.get('email', default='')
-        client.query(
+        result = client.query(
             q.create(
                 q.ref('collections/Users'),
                 {
@@ -38,6 +38,7 @@ def hello():
                 }
             )
         )
+        print(result['ref'].id())
         flash("New contact added successfully", category="success")
         return redirect('/')
     else:
